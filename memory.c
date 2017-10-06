aa5aab63cd0c463c8117c23ab0b9f06b83b3e7b3
Paulbel <pavelsinelnikovbel@gmail.com>
2017-10-06 14:19:57 +0300

    smth wrong

--- memory.c
+++ memory.c
@@ -2,29 +2,8 @@
 #include "mmemory.h"
 
 #include <stdlib.h>
+#include "memory.h"
 
-int _init(int n, int szPage);
-
-void page_init(int n, int pageSize);
-
-void block_init();
-
-struct block *get_block_by_va(VA ptr, int size);
-
-
-struct pageDescription {
-    struct page *page;
-    struct pageInfo pageInfo;
-};
-
-
-struct memory {
-    struct page *pages;
-    int pageNumber;
-    int pageSize;
-    int *isUsed;//1 if used, 0 if not
-    int freeMemorySize;
-};
 
 /*
 struct block {
@@ -123,29 +102,96 @@ struct pageInfo {
 @retval	-1	неверные параметры
 @retval	1	неизвестная ошибка
 **/
+int _init(int n, int szPage);
 
+void page_init(int n, int pageSize);
+
+void block_init();
+
+struct block *get_block_by_va(VA ptr, int size);
+
+
+struct pageDescription {
+    struct page *page;
+    struct pageInfo *pageInfo;
+};
+
+
+struct memory {
+    struct page *pages;
+    int pageNumber;
+    int freeMemorySize;
+};
+
+
+struct hardDrive {
+    struct page *pages;
+    int capacity;
+};
+
+
+struct pageTable {
+    VA addr;
+    struct pageDescription *descriptors;
+    int pageNumber;
+    struct pageTable *nextTable;
+};
+
+
+struct pageTable *firstTable = NULL;
 struct memory *memory;
+struct hardDrive *hdd;
+const int OUT_OF_MEMORY_ERROR;
+unsigned const int PAGE_SIZE = 2;
+unsigned const int BLOCK_SIZE = 2;
+unsigned const int PAGES_ON_HDD_NUMBER = 20;
+unsigned const int BIT_CAPACITY = 16;
 //struct page *memory;
 //int freeMemorySize;
 //int pageSize;
 //int pageNumber;
 
-int getFreeMemoryIndex() {
-    for (int adress = 0; adress < memory->pageNumber; adress++) {
-        if (memory->isUsed[adress] == 0) {
-            return adress;//adress of the first free page in physical memory
+
+void toDecimalNumber(VA addr, int * p, int * i){
+    i = 0;
+    int pageNumber =0;
+
+    for (int rank = BIT_CAPACITY/2; rank >= 0; rank--){
+        if(addr[rank]=='1'){
+           // pageNumber+=pow(2,rank);
         }
     }
-    return -1;//if there are no free pages in physical memory
-}
-
-int pageIsFreeByAdress(int adress) {
-    if (memory->isUsed[adress] == 0) {
-        return 1;//if page in this adress is free
+    p = pageNumber;
+/*
+    for (int rank = BIT_CAPACITY/2; rank >= BIT_CAPACITY/2; rank--){
+        if(addr[])
     }
-    return 0;//if it isnt free
+*/
+
 }
 
+
+
+
+
+int getIndexNextFreeMemoryPage(int pageNumber) {
+    for (int index = pageNumber; index < memory->pageNumber; index++) {
+        if (memory->pages[index].pFirstUse == NULL) {
+            return index;
+        }
+    }
+    return -1;
+}
+
+
+int getIndexNextFreeHDDPage(int pageNumber) {
+    for (int index = pageNumber; index < hdd->capacity; index++) {
+        if (memory->pages[index].pFirstUse == NULL) {
+            return index;
+        }
+    }
+    return -1;
+}
 
 int _init(int n, int szPage) {
     if (szPage <= 0 || n <= 0) {
@@ -153,111 +199,82 @@ int _init(int n, int szPage) {
     }
 
     memory = malloc(sizeof(memory));
-    memory->pages = calloc(n, sizeof(struct page));
-    memory->pageNumber = n;
-    memory->pageSize = szPage;
     memory->freeMemorySize = n * szPage;
-    for (int pageIndex = 0; pageIndex < memory->pageNumber; ++pageIndex) {
+    memory->pageNumber = n;
+    memory->pages = calloc(n, sizeof(struct page));
+    hdd = malloc(sizeof(struct hardDrive));
+    hdd->pages = calloc(PAGES_ON_HDD_NUMBER, sizeof(struct page));
+    hdd->capacity = PAGES_ON_HDD_NUMBER;
+    struct block *prevBlock = NULL;
+    for (int pageIndex = 0; pageIndex < memory->pageNumber; pageIndex++) {
         struct page page;
-
-        page.pFirstFree = malloc(sizeof(struct block));
-        page.pFirstFree->pNext = NULL;
-        page.pFirstFree->szBlock = szPage;
-
-
-        page.pFirstUse = malloc(sizeof(struct block));
-        page.pFirstUse->szBlock = 0;
-        page.pFirstUse->pNext = NULL;
-
-        page.maxSizeFreeBlock = szPage;
-        memory->pages[pageIndex] = page;
-        memory->isUsed = 0;
+        for (int blockIndex = 0; blockIndex < PAGE_SIZE; blockIndex++) {
+            struct block *block = malloc(sizeof(struct block));
+            if (blockIndex == 0) {
+                page.pFirstFree = block;
+            }
+            block->szBlock = BLOCK_SIZE;
+            if (prevBlock != NULL) {
+                prevBlock->pNext = block;
+            }
+            prevBlock = block;
+        }
+        page.maxSizeFreeBlock = page.pFirstFree->szBlock;
+        page.pFirstUse = NULL;
     }
-    memory->isUsed = calloc(n, sizeof(int));
-
-
     VA *p;
     _malloc(p, 3);
 }
 
 
 int _malloc(VA *ptr, size_t szBlock) {
-
     if (szBlock <= 0 || !ptr)
         return -1;
     if (szBlock > memory->freeMemorySize)
         return -2;
 
-    memory->freeMemorySize -= szBlock;
-
-    int sizeLeft = szBlock;
+    struct pageTable *table = malloc(sizeof(struct pageTable));
 
 
-    int pageNumber = sizeLeft / memory->pageSize;
-    if ((sizeLeft % memory->pageSize) > 0) {
-        pageNumber++;
-    }
-    //struct pageDescription *pageDescription = calloc(pageNumber, sizeof(struct pageDescription));
-
-
-    struct pageDescription **pageTable = calloc(pageNumber, sizeof(struct pageDescription));
-
-    for (int number = 0; number < pageNumber; number++) {
-        int freePageNumber = getFreeMemoryIndex();
-
-        pageTable[number] = (struct pageDescription *)malloc(sizeof(struct pageDescription));
-        pageTable[number] = malloc(sizeof(struct pageDescription));
-        pageTable[number]->page = &memory->pages[freePageNumber];
-        memory->isUsed[freePageNumber] = 1;
-        //pageDescription[number] = malloc(sizeof(struct pageDescription));
-    }
-    struct block * prevBlock = NULL;
-    for (int pageIndex = 0; pageIndex < pageNumber; ++pageIndex) {
-        struct page *page = pageTable[pageIndex]->page;
-        if (sizeLeft >= memory->pageSize) {
-            page->pFirstUse->szBlock = memory->pageSize;
-            page->pFirstFree->szBlock = 0;
-            sizeLeft -= memory->pageSize;
-        } else {
-            page->pFirstUse->szBlock = sizeLeft;
-            page->pFirstFree->szBlock = memory->pageSize - sizeLeft;
+    if (firstTable == NULL) {
+        firstTable = table;
+    } else {
+        struct pageTable *prevTable = firstTable;
+        while (prevTable->nextTable != NULL) {
+            prevTable = prevTable->nextTable;
         }
-        if(prevBlock!=NULL){
-            prevBlock->pNext = page->pFirstUse;
-        }
-        prevBlock = page->pFirstUse;
+        prevTable->nextTable = table;
     }
 
-    // while (sizeLeft >= 0) {
-    // pageDescription[]
-    //sizeLeft -= memory->pageSize;
-    //struct pageDescription *pageDescription = malloc(sizeof(struct pageDescription));
-    //pageDescription->page.maxSizeFreeBlock = 3;
+    int pageSize = PAGE_SIZE * BLOCK_SIZE;
 
-    //pageDescription = (struct pageDescription *) calloc(n, (sizeof(struct pageDescription)));
-    //pageData = (struct data *) calloc(n, sizeof(struct data));
-/*        for (int i = 0; i < n; i++) {
-            struct pageDescription *pageD = (struct pageDescription *) malloc(sizeof(struct pageDescription));
-            pageDes[i] = pageD;
-            pageD->page.maxSizeFreeBlock = NULL;
-            pageD->page.pFirstFree = first_block;
-            pageD->page.pFirstUse = NULL;
-            pageD->pageI.isUse = '0';
-            pageD->pageI.offsetPage = create_offset(i * SIZE_OF_PAGE);
-            pageDes[i] = malloc(sizeof(struct pageDescription));
+    if (szBlock % pageSize == 0) {
+        table->pageNumber = szBlock / pageSize;
+    } else {
+        table->pageNumber = szBlock / pageSize + 1;
+    }
 
 
-            struct data *pageData_ = malloc(sizeof(struct data));;
-            pageData_->numberOfPage = i;
-            pageData_->offset = create_offset(i * SIZE_OF_PAGE);
-            pageData_->someData = malloc(sizeof(SIZE_OF_PAGE));
-            pageData_->someData[SIZE_OF_PAGE] = '\0';
-            pageData[i] = pageData_;
-        }*/
-    // }
+    table->descriptors = calloc(table->pageNumber, sizeof(struct pageDescription));
+    int nextPageInMemoryNumber = 0;
+    int nextPageInHDDNumber = 0;
+/*    for (int descriptorIndex = 0; descriptorIndex < table->pageNumber; descriptorIndex++) {
+        nextPageInMemoryNumber = getIndexNextFreeMemoryPage(nextPageInMemoryNumber);
+        if (nextPageInMemoryNumber == -1) {
+            nextPageInHDDNumber = getIndexNextFreeHDDPage(nextPageInHDDNumber);
+            if (nextPageInHDDNumber == -1) {
+                return OUT_OF_MEMORY_ERROR;
+            } else{
+                table->
+            }
+        }
+        struct pageDescription description = table->descriptors[descriptorIndex];
+        description.page = &memory->pages[nextPageInMemoryNumber];
+        description.pageInfo = malloc(sizeof(struct pageInfo));
+
+    }*/
 
 
-    return 0;
 }
 
 
--- test.c
+++ test.c
@@ -1,10 +1,34 @@
 #include "stdafx.h"
 #include "mmemory.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 int main( )
 {
     printf("Hello, World!\n");
-    _init(10,2);
+    //_init(10,2);
+    int * p ;
+    int * i ;
+    VA addr;
+    addr = calloc(16, sizeof(char));
+
+    addr[0] = '1';
+    addr[1] = '1';
+    addr[2] = '1';
+    addr[3] = '1';
+    addr[4] = '1';
+    addr[5] = '1';
+    addr[6] = '1';
+    addr[7] = '1';
+    addr[8] = '1';
+    addr[9] = '1';
+    addr[10] = '1';
+    addr[11] = '1';
+    addr[12] = '1';
+    addr[13] = '1';
+    addr[14] = '1';
+    addr[15] = '1';
+
+    //toDecimalNumber(addr,p,i);
+    printf(p);
     return 0;
 }
